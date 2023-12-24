22135d82f755a6e6cbbdfacd98683cb9158500f8
Ethan Hackney <ehackneyed@gmail.com>
2023-12-24 10:18:46 -0800

    implement automatic pipe

--- main.c
+++ main.c
@@ -8,240 +8,19 @@ static void Exec(char *prog);
 static pid_t Fork(void);
 static void Close(int fd);
 static void Dup(int fd);
+static void Run(char **cmds, int nr_cmds);
 
 int
 main(void)
 {
-        pid_t last;
-        int pipefds[2];
-        int pipefds2[2];
-        int infd;
+        char *cmds[] = {
+                "ls",
+                "head",
+                "wc",
+        };
+        int nr_cmds = sizeof(cmds) / sizeof(*cmds);
 
-        /**
-         * File descriptor table
-         * =====================
-         *
-         * [ 0 ]----> stdin
-         * [ 1 ]----> stdout
-         * [ 2 ]----> stderr
-         * [...]
-         * [ p ]----> [ ][ ][ ][ ]
-         * [ p ]-------^
-         */
-        Pipe(pipefds);
-
-        if (Fork() == 0) {
-                /**
-                 * File descriptor table
-                 * =====================
-                 *
-                 * [ 0 ]----> stdin
-                 * [ 1 ]----x
-                 * [ 2 ]----> stderr
-                 * [...]
-                 * [ p ]----> [ ][ ][ ][ ]
-                 * [ p ]-------^
-                 */
-                Close(STDOUT_FILENO);
-
-                /**
-                 * File descriptor table
-                 * =====================
-                 *
-                 * [ 0 ]----> stdin
-                 * [ 1 ]----------------+
-                 * [ 2 ]----> stderr    |
-                 * [...]                V
-                 * [ p ]----> [ ][ ][ ][ ]
-                 * [ p ]-------^
-                 */
-                Dup(pipefds[1]);
-
-                /**
-                 * File descriptor table
-                 * =====================
-                 *
-                 * [ 0 ]----> stdin
-                 * [ 1 ]----------------+
-                 * [ 2 ]----> stderr    |
-                 * [...]                V
-                 * [ p ]----x [ ][ ][ ][ ]
-                 * [ p ]----x
-                 */
-                Close(pipefds[0]);
-                Close(pipefds[1]);
-
-                Exec("ls");
-        }
-
-        /**
-         * File descriptor table
-         * =====================
-         *
-         * [ 0 ]----> stdin
-         * [ 1 ]----> stdout
-         * [ 2 ]----> stderr
-         * [...]
-         * [ p ]----> [ ][ ][ ][ ]
-         * [ p ]-------^
-         * [ p ]----> [ ][ ][ ][ ]
-         * [ p ]-------^
-         */
-        Pipe(pipefds2);
-
-        if (Fork() == 0) {
-                /**
-                 * File descriptor table
-                 * =====================
-                 *
-                 * [ 0 ]----x
-                 * [ 1 ]----x
-                 * [ 2 ]----> stderr
-                 * [...]
-                 * [ p ]----> [ ][ ][ ][ ]
-                 * [ p ]-------^
-                 * [ p ]----> [ ][ ][ ][ ]
-                 * [ p ]-------^
-                 */
-                Close(STDIN_FILENO);
-                Close(STDOUT_FILENO);
-
-                /**
-                 * File descriptor table
-                 * =====================
-                 *
-                 * [ 0 ]----------------+
-                 * [ 1 ]----------------|---+
-                 * [ 2 ]----> stderr    |   |
-                 * [...]       V--------+   |
-                 * [ p ]----> [ ][ ][ ][ ]  |
-                 * [ p ]-------^            |
-                 * [ p ]----> [ ][ ][ ][ ]  |
-                 * [ p ]-------^------------+
-                 */
-                Dup(pipefds[0]);
-                Dup(pipefds2[1]);
-
-                /**
-                 * File descriptor table
-                 * =====================
-                 *
-                 * [ 0 ]----------------+
-                 * [ 1 ]----------------|---+
-                 * [ 2 ]----> stderr    |   |
-                 * [...]       V--------+   |
-                 * [ p ]----x [ ][ ][ ][ ]  |
-                 * [ p ]----x               |
-                 * [ p ]----> [ ][ ][ ][ ]  |
-                 * [ p ]-------^------------+
-                 */
-                Close(pipefds[0]);
-                Close(pipefds[1]);
-
-                /**
-                 * File descriptor table
-                 * =====================
-                 *
-                 * [ 0 ]----------------+
-                 * [ 1 ]----------------|---+
-                 * [ 2 ]----> stderr    |   |
-                 * [...]       V--------+   |
-                 * [ p ]----x [ ][ ][ ][ ]  |
-                 * [ p ]----x               |
-                 * [ p ]----x [ ][ ][ ][ ]  |
-                 * [ p ]----x  ^------------+
-                 */
-                Close(pipefds2[0]);
-                Close(pipefds2[1]);
-
-                Exec("head");
-        }
-
-        /**
-         * File descriptor table
-         * =====================
-         *
-         * [ 0 ]----> stdin
-         * [ 1 ]----> stdout
-         * [ 2 ]----> stderr
-         * [...]
-         * [ p ]----x [ ][ ][ ][ ]
-         * [ p ]----x
-         * [ p ]----> [ ][ ][ ][ ]
-         * [ p ]-------^
-         */
-        Close(pipefds[0]);
-        Close(pipefds[1]);
-
-        last = Fork();
-        if (last == 0) {
-                /**
-                 * File descriptor table
-                 * =====================
-                 *
-                 * [ 0 ]----x
-                 * [ 1 ]----> stdout
-                 * [ 2 ]----> stderr
-                 * [...]
-                 * [ p ]----x [ ][ ][ ][ ]
-                 * [ p ]----x
-                 * [ p ]----> [ ][ ][ ][ ]
-                 * [ p ]-------^
-                 */
-                Close(STDIN_FILENO);
-
-                /**
-                 * File descriptor table
-                 * =====================
-                 *
-                 * [ 0 ]----------------------+
-                 * [ 1 ]----> stdout          |
-                 * [ 2 ]----> stderr          |
-                 * [...]                      |
-                 * [ p ]----x [ ][ ][ ][ ]    |
-                 * [ p ]----x  V--------------+
-                 * [ p ]----> [ ][ ][ ][ ]
-                 * [ p ]-------^
-                 */
-                Dup(pipefds2[0]);
-
-                /**
-                 * File descriptor table
-                 * =====================
-                 *
-                 * [ 0 ]----------------------+
-                 * [ 1 ]----> stdout          |
-                 * [ 2 ]----> stderr          |
-                 * [...]                      |
-                 * [ p ]----x [ ][ ][ ][ ]    |
-                 * [ p ]----x  V--------------+
-                 * [ p ]----x [ ][ ][ ][ ]
-                 * [ p ]----x
-                 */
-                Close(pipefds2[0]);
-                Close(pipefds2[1]);
-
-                Exec("wc");
-        }
-
-        /**
-         * File descriptor table
-         * =====================
-         *
-         * [ 0 ]----> stdin
-         * [ 1 ]----> stdout
-         * [ 2 ]----> stderr
-         * [...]
-         * [ p ]----x [ ][ ][ ][ ]
-         * [ p ]----x
-         * [ p ]----x [ ][ ][ ][ ]
-         * [ p ]----x
-         */
-        Close(pipefds2[0]);
-        Close(pipefds2[1]);
-
-        if (waitpid(last, NULL, 0) < 0)
-                err(EX_OSERR, "waitpid");
+        Run(cmds, nr_cmds);
 }
 
 static void
@@ -281,4 +60,82 @@ Dup(int fd)
 {
         if (dup(fd) < 0)
                 err(EX_OSERR, "dup");
+}
+
+static void
+Run(char **cmds, int nr_cmds)
+{
+        pid_t last;
+        int first;
+        int infd;
+        int c;
+
+        if (nr_cmds == 1) {
+                last = Fork();
+                if (last == 0)
+                        Exec(cmds[0]);
+
+                if (waitpid(last, NULL, 0) < 0)
+                        err(EX_OSERR, "waitpid");
+
+                return;
+        }
+
+        first = 1;
+        infd = STDIN_FILENO;
+        for (c = 0; c < nr_cmds; c++) {
+                if (c == nr_cmds - 1) {
+                        last = Fork();
+                        if (last == 0) {
+                                Close(STDIN_FILENO);
+
+                                Dup(infd);
+
+                                Close(infd);
+
+                                Exec(cmds[c]);
+                        }
+                        Close(infd);
+                } else if (first) {
+                        int pipefds[2];
+
+                        Pipe(pipefds);
+                        if (Fork() == 0) {
+                                Close(STDOUT_FILENO);
+
+                                Dup(pipefds[1]);
+
+                                Close(pipefds[0]);
+                                Close(pipefds[1]);
+
+                                Exec(cmds[c]);
+                        }
+
+                        infd = pipefds[0];
+                        Close(pipefds[1]);
+                        first = 0;
+                } else {
+                        int pipefds[2];
+
+                        Pipe(pipefds);
+                        if (Fork() == 0) {
+                                Close(STDIN_FILENO);
+                                Close(STDOUT_FILENO);
+
+                                Dup(infd);
+                                Dup(pipefds[1]);
+
+                                Close(pipefds[0]);
+                                Close(pipefds[1]);
+
+                                Exec(cmds[c]);
+                        }
+
+                        infd = pipefds[0];
+                        Close(pipefds[1]);
+                }
+        }
+
+        if (waitpid(last, NULL, 0) < 0)
+                err(EX_OSERR, "waitpid");
 }
